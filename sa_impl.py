18bc621030fff296820b83f0fd41bf8a25efd082
deepanshut041 <deepanshut041@gmail.com>
2018-04-23 06:51:27 +0530

    added sa implementation

--- sa_impl.py
+++ sa_impl.py
@@ -1,83 +1,124 @@
+## Generate a contour plot
+# Import some other libraries that we'll need
+# matplotlib and numpy packages must also be installed
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 import random
 import math
-LIMIT = 100000
 
-def update_temperature(T, k):
-    return T - 0.001
+# define objective function
+def f(x):
+    x1 = x[0]
+    x2 = x[1]
+    obj = 0.2 + x1**2 + x2**2 - 0.1*math.cos(6.0*3.1415*x1) - 0.1*math.cos(6.0*3.1415*x2)
+    return obj
 
-def get_neighbors(i, L):
-    assert L > 1 and i >= 0 and i < L
-    if i == 0:
-        return [1]
-    elif i == L - 1:
-        return [L - 2]
-    else:
-        return [i - 1, i + 1]
+# Start location
+x_start = [0.8, -0.5]
 
-def make_move(x, A, T):
-    # nhbs = get_neighbors(x, len(A))
-    # nhb = nhbs[random.choice(range(0, len(nhbs)))]
-    nhb = random.choice(range(0, len(A))) # choose from all points
+# Design variables at mesh points
+i1 = np.arange(-1.0, 1.0, 0.01)
+i2 = np.arange(-1.0, 1.0, 0.01)
+x1m, x2m = np.meshgrid(i1, i2)
+fm = np.zeros(x1m.shape)
+for i in range(x1m.shape[0]):
+    for j in range(x1m.shape[1]):
+        fm[i][j] = 0.2 + x1m[i][j]**2 + x2m[i][j]**2 \
+             - 0.1*math.cos(6.0*3.1415*x1m[i][j]) \
+             - 0.1*math.cos(6.0*3.1415*x2m[i][j])
 
-    delta = A[nhb] - A[x]
+# Create a contour plot
+plt.figure()
+# Specify contour lines
+#lines = range(2,52,2)
+# Plot contours
+CS = plt.contour(x1m, x2m, fm)#,lines)
+# Label contours
+plt.clabel(CS, inline=1, fontsize=10)
+# Add some text to the plot
+plt.title('Non-Convex Function')
+plt.xlabel('x1')
+plt.ylabel('x2')
 
-    if delta < 0:
-        return nhb
-    else:
-        p = math.exp(-delta / T)
-        return nhb if random.random() < p else x
+##################################################
+# Simulated Annealing
+##################################################
+# Number of cycles
+n = 50
+# Number of trials per cycle
+m = 50
+# Number of accepted solutions
+na = 0.0
+# Probability of accepting worse solution at the start
+p1 = 0.7
+# Probability of accepting worse solution at the end
+p50 = 0.001
+# Initial temperature
+t1 = -1.0/math.log(p1)
+# Final temperature
+t50 = -1.0/math.log(p50)
+# Fractional reduction every cycle
+frac = (t50/t1)**(1.0/(n-1.0))
+# Initialize x
+x = np.zeros((n+1,2))
+x[0] = x_start
+xi = np.zeros(2)
+xi = x_start
+na = na + 1.0
+# Current best results so far
+xc = np.zeros(2)
+xc = x[0]
+fc = f(xi)
+fs = np.zeros(n+1)
+fs[0] = fc
+# Current temperature
+t = t1
+# DeltaE Average
+DeltaE_avg = 0.0
+for i in range(n):
+    print('Cycle: ' + str(i) + ' with Temperature: ' + str(t))
+    for j in range(m):
+        # Generate new trial points
+        xi[0] = xc[0] + random.random() - 0.5
+        xi[1] = xc[1] + random.random() - 0.5
+        # Clip to upper and lower bounds
+        xi[0] = max(min(xi[0],1.0),-1.0)
+        xi[1] = max(min(xi[1],1.0),-1.0)
+        DeltaE = abs(f(xi)-fc)
+        if (f(xi)>fc):
+            # Initialize DeltaE_avg if a worse solution was found
+            #   on the first iteration
+            if (i==0 and j==0): DeltaE_avg = DeltaE
+            # objective function is worse
+            # generate probability of acceptance
+            p = math.exp(-DeltaE/(DeltaE_avg * t))
+            # determine whether to accept worse point
+            if (random.random()<p):
+                # accept the worse solution
+                accept = True
+            else:
+                # don't accept the worse solution
+                accept = False
+        else:
+            # objective function is lower, automatically accept
+            accept = True
+        if (accept==True):
+            # update currently accepted solution
+            xc[0] = xi[0]
+            xc[1] = xi[1]
+            fc = f(xc)
+            # increment number of accepted solutions
+            na = na + 1.0
+            # update DeltaE_avg
+            DeltaE_avg = (DeltaE_avg * (na-1.0) +  DeltaE) / na
+    # Record the best x values at the end of every cycle
+    x[i+1][0] = xc[0]
+    x[i+1][1] = xc[1]
+    fs[i+1] = fc
+    # Lower the temperature for next cycle
+    t = frac * t
 
-def simulated_annealing(A):
-    L = len(A)
-    x0 = random.choice(range(0, L))
-    T = 1.
-    k = 1
-
-    x = x0
-    x_best = x0
-
-    while T > 1e-3:
-        x = make_move(x, A, T)
-        if(A[x] < A[x_best]):
-            x_best = x
-        T = update_temperature(T, k)
-        k += 1
-
-    print("iterations:", k)
-    return x, x_best, x0
-
-def isminima_local(p, A):
-    return all(A[p] < A[i] for i in get_neighbors(p, len(A)))
-
-def func(x):
-    return math.sin((2 * math.pi / LIMIT) * x) + 0.001 * random.random()
-
-def initialize(L):
-    return map(func, range(0, L))
-
-def main():
-    A = initialize(LIMIT)
-
-    local_minima = []
-    for i in range(0, LIMIT):
-        if(isminima_local(i, A)):
-            local_minima.append([i, A[i]])
-
-    x = 0
-    y = A[x]
-    for xi, yi in enumerate(A):
-        if yi < y:
-            x = xi
-            y = yi
-    global_minumum = x
-
-    print("number of local minima: %d" % (len(local_minima)))
-    print("global minimum @%d = %0.3f" % (global_minumum, A[global_minumum]))
-
-    x, x_best, x0 = simulated_annealing(A)
-    print("Solution is @%d = %0.3f" % (x, A[x]))
-    print("Best solution is @%d = %0.3f" % (x_best, A[x_best]))
-    print("Start solution is @%d = %0.3f" % (x0, A[x0]))
-
-
-main()
+# print solution
+print('Best solution: ' + str(xc))
+print('Best objective: ' + str(fc))
