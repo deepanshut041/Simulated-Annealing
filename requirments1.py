4bc8209bbf744e065e25c5d3d8a68e8536335e38
deepanshut041 <deepanshut041@gmail.com>
2018-04-24 01:01:07 +0530

    Rearrangend files for requirment 1 a rearrange files for requirment 2

--- requirments1.py
+++ requirments1.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy import ndimage
 from network_helper import *
+import sys
 
 np.random.seed(1)
 
@@ -31,19 +32,6 @@ def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, 
  
         # Update parameters.
         parameters = update_parameters(parameters, grads, learning_rate)
-                
-        # Print the cost every 100 training example
-        if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" % (i, cost))
-        if print_cost and i % 100 == 0:
-            costs.append(cost)
-            
-    # plot the cost
-    plt.plot(np.squeeze(costs))
-    plt.ylabel('cost')
-    plt.xlabel('iterations (per tens)')
-    plt.title("Learning rate =" + str(learning_rate))
-    plt.show()
     
     return parameters
 
@@ -51,23 +39,11 @@ def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, 
 # Loading dataset
 train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
 
-# Checking dataset
-# index = 10
-# plt.imshow(train_x_orig[index])
-# print ("y = " + str(train_y[0, index]) + ". It's a " + classes[train_y[0, index]].decode("utf-8") + " picture.")
-
 # Explore your dataset 
 m_train = train_x_orig.shape[0]
 num_px = train_x_orig.shape[1]
 m_test = test_x_orig.shape[0]
 
-print ("Number of training examples: " + str(m_train))
-print ("Number of testing examples: " + str(m_test))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_x_orig shape: " + str(train_x_orig.shape))
-print ("train_y shape: " + str(train_y.shape))
-print ("test_x_orig shape: " + str(test_x_orig.shape))
-print ("test_y shape: " + str(test_y.shape))
 
 
 # Reshape the training and test examples 
@@ -78,18 +54,25 @@ test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 train_x = train_x_flatten / 255.
 test_x = test_x_flatten / 255.
 
-
+weight_single_cell = 4
+print("Size required by one weight", weight_single_cell)
 print ("train_x's shape: " + str(train_x.shape))
-print ("test_x's shape: " + str(test_x.shape))
+print("Total size of model will be require no weight in a layer * size required by one weight * Number of layers")
+print("Let we have an input layer of 12288 and two output layer of 50 and 20 weights than total size size ", 12288 * 50 * 20 * weight_single_cell)
+# print ("test_x's shape: " + str(test_x.shape))
 
 ### CONSTANTS DEFINING THE MODEL ####
 n_x = 12288    # num_px * num_px * 3
 n_h = int(n_x / 4)
 n_y = 1
-layers_dims = (n_x, n_h , n_y)
+layers_dims = (n_x, 50, 20 , n_y)
 
-parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations=2500, print_cost=True)
+parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations=200, print_cost=True)
+print("Total size of model is equal to sum of size of its all parameters")
+print("Size of input layer: ", sys.getsizeof(parameters['W1']))
+print("Size of hidden layer 1: ", sys.getsizeof(parameters['W2']))
+print("Size of layer hidden layer 2 : ", sys.getsizeof(parameters['W3']))
+print("Total size : " , sys.getsizeof(parameters['W1']) + sys.getsizeof(parameters['W2']) + sys.getsizeof(parameters['W3']))
 
-pred_train = predict(train_x, train_y, parameters)
-
-pred_test = predict(test_x, test_y, parameters)
+print("Calculated total size is : " , 12288 * 50 * 20 * weight_single_cell)
+print("Total size is : " , 12288 * 50 * 20 * weight_single_cell)
